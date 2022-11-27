f1858b09442d1c7f9d8a199750fd4d68dbce12b4
manliot <manliot@uninorte.edu.co>
2022-11-27 16:48:03 -0500

    feat: Principal Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 //import './App.css';
 
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoCounter } from "./TodoCounter";
+import { TodoItem } from "./TodoItem";
+import { TodoList } from "./TodoList";
+import { TodoSearch } from "./TodoSearch";
+
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Tomar el curso de intro a React', completed: false },
+  { text: 'Llorar con la llorona', completed: false }
+]
+
 function App() {
   return (
-    <div>
-      Todo
-    </div>
+    <>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {
+          todos.map(todo => (
+            <TodoItem text={todo.text} completed={todo.completed} key={todo.text}/>
+          ))
+        }
+      </TodoList>
+      <CreateTodoButton />
+
+
+    </>
   );
 }
 
