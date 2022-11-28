1d42dc2328d1c6ecd552435183a1c9c1368503ad
manliot <manliot@uninorte.edu.co>
2022-11-27 22:37:30 -0500

    style: create tasks styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { TodoList } from "./TodoList";
 import { TodoSearch } from "./TodoSearch";
 
 const todos = [
-  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cortar cebolla', completed: true },
   { text: 'Tomar el curso de intro a React', completed: false },
   { text: 'Llorar con la llorona', completed: false }
 ]
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './TodoItem.css'
 
 function TodoItem(props) {
     return (
-        <li>
-            <span>C</span>
-            <p>{props.text}</p>
-            <span>X</span>
+        <li className='item'>
+            <span>{props.completed === true ? '✅' : '☑️'}</span>
+            <p className={props.completed === true ? 'item-text' : ''}>{props.text}</p>
+            <span>❌</span>
         </li>
     )
 }
