e04a41bbba111283b732c8faf861eeda969fc320
manliot <manliot@uninorte.edu.co>
2022-12-27 22:19:04 -0500

    change icons in the item component

--- src/TodoItem/index.js
+++ src/TodoItem/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { CheckIcon } from '../Assets/CheckIcon'
+import { DeleteIcon } from '../Assets/DeleteIcon'
 import './TodoItem.css'
 
 function TodoItem(props) {
@@ -8,15 +10,15 @@ function TodoItem(props) {
             <span
                 className='check-btn-item'
                 onClick={props.onComplete}
-                >
-                {props.completed === true ? '✅' : '☑️'}
+            >
+                <CheckIcon completed={props.completed} w='20px' h='20px' />
             </span>
             <p className={`item-txt ${props.completed === true ? 'dash-text' : ''}`}>{props.text}</p>
             <span
                 className='delete-btn-item'
                 onClick={props.onDelete}
             >
-                ❌
+                <DeleteIcon w='20px' h='20px' />
             </span>
         </li>
     )
